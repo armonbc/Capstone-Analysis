caf3d8741b682275935036921d5feb4b73b3e0dd
Armon C <121739826+armonbc@users.noreply.github.com>
2023-03-23 20:42:29 +0800

    Update all_zip_downloader.py

--- Pythonscripts/all_zip_downloader.py
+++ Pythonscripts/all_zip_downloader.py
@@ -18,10 +18,28 @@ xml_response = requests.get(url)
 # Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(xml_response.content, 'xml')
 
+# Select only the Zip files for a certain period range
+start_date = "202203"
+end_date = "202302"
+
+year = int(start_date[:4])
+month = int(start_date[4:])
+dates = []
+
+while True:
+    date_str = f"{year}{month:02}"
+    dates.append(date_str+"-divvy-tripdata.zip")
+    if date_str == end_date:
+        break
+    month += 1
+    if month > 12:
+        year += 1
+        month = 1
+
 # loop through each object and print its key
 for content in soup.find_all('Contents'):
     key = content.find('Key').text
-    if key and key.endswith(".zip"):
+    if key and key in dates:
        file_abs = f"{url}/{key}"
        print(f"Downloading ... {file_abs}")
        
